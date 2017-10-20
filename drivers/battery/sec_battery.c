ddd4d12905cf92cbfb9883221d30be0aa7d112e8
gamersab <abdulraffay502@gmail.com>
2017-10-20 14:35:27 -0400

    Charging fix

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -19,6 +19,11 @@ extern int get_hw_rev();
 extern int lpcharge;
 extern void sec_bat_initial_check(void);
 
+#ifdef CONFIG_SUPER_CHARGER_CONTROL
+#include "super_charger_control.h"
+int cust_ac_lev = AC_DEFAULT_LEVEL; 
+#endif 
+ 
 static struct device_attribute sec_battery_attrs[] = {
 	SEC_BATTERY_ATTR(batt_reset_soc),
 	SEC_BATTERY_ATTR(batt_read_raw_soc),
@@ -2174,8 +2179,15 @@ static void sec_bat_get_battery_info(
 	value.intval = SEC_BATTEY_CURRENT_MA;
 	psy_do_property(battery->pdata->fuelgauge_name, get,
 		POWER_SUPPLY_PROP_CURRENT_NOW, value);
-	battery->current_now = value.intval;
-
+	
+#ifdef CONFIG_SUPER_CHARGER_CONTROL
+        if(super_charge) 
+        battery->current_now = cust_ac_lev;
+        else
+ 	battery->current_now = value.intval;
+#else
+        battery->current_now = value.intval; 
+#endif
 	value.intval = SEC_BATTEY_CURRENT_MA;
 	psy_do_property(battery->pdata->fuelgauge_name, get,
 		POWER_SUPPLY_PROP_CURRENT_AVG, value);
@@ -2184,8 +2196,14 @@ static void sec_bat_get_battery_info(
 	/* input current limit in charger */
 	psy_do_property(battery->pdata->charger_name, get,
 		POWER_SUPPLY_PROP_CURRENT_MAX, value);
+#ifdef CONFIG_SUPER_CHARGER_CONTROL
+        if(super_charge)  
+        battery->current_max = cust_ac_lev;
+        else
+        battery->current_max = value.intval;
+#else
 	battery->current_max = value.intval;
-
+#endif
 	sec_bat_get_temperature_info(battery);
 
 
@@ -2519,17 +2537,8 @@ static void sec_bat_monitor_work(
 		if ((battery->status == POWER_SUPPLY_STATUS_DISCHARGING) &&
 			(battery->ps_enable != true)) {
 			if ((unsigned long)(c_ts.tv_sec - old_ts.tv_sec) < 10 * 60) {
-				union power_supply_propval value;
-
-				psy_do_property(battery->pdata->fuelgauge_name, get,
-					POWER_SUPPLY_PROP_VOLTAGE_NOW, value);
-				battery->voltage_now = value.intval;
-				sec_bat_get_temperature_info(battery);
-
-				power_supply_changed(&battery->psy_bat);
-
-				pr_info("Skip monitor work(%ld, Vnow:%dmV, Tbat:%d)\n",
-					c_ts.tv_sec - old_ts.tv_sec, battery->voltage_now, battery->temperature);
+				pr_info("Skip monitor_work(%ld)\n",
+						c_ts.tv_sec - old_ts.tv_sec);
 				goto skip_monitor;
 			}
 		}
@@ -2627,17 +2636,17 @@ continue_monitor:
 			"%s: battery->stability_test(%d), battery->eng_not_full_status(%d)\n",
 			__func__, battery->stability_test, battery->eng_not_full_status);
 #endif
-	if (battery->store_mode && !lpcharge && (battery->cable_type != POWER_SUPPLY_TYPE_BATTERY)) {
+	if (battery->store_mode && battery->cable_type != POWER_SUPPLY_TYPE_BATTERY) {
 
 		dev_info(battery->dev,
 			"%s: @battery->capacity = (%d), battery->status= (%d), battery->store_mode=(%d)\n",
 			__func__, battery->capacity, battery->status, battery->store_mode);
 
-		if ((battery->capacity >= STORE_MODE_CHARGING_MAX) && (battery->status == POWER_SUPPLY_STATUS_CHARGING)) {
+		if ((battery->capacity >= 35) && (battery->status == POWER_SUPPLY_STATUS_CHARGING)) {
 			battery->status = POWER_SUPPLY_STATUS_DISCHARGING;
 			sec_bat_set_charge(battery, false);
 		}
-		if ((battery->capacity <= STORE_MODE_CHARGING_MIN) && (battery->status == POWER_SUPPLY_STATUS_DISCHARGING)) {
+		if ((battery->capacity <= 30) && (battery->status == POWER_SUPPLY_STATUS_DISCHARGING)) {
 			battery->status = POWER_SUPPLY_STATUS_CHARGING;
 			sec_bat_set_charge(battery, true);
 		}
@@ -5414,11 +5423,7 @@ static int sec_battery_probe(struct platform_device *pdev)
 	battery->cable_type = POWER_SUPPLY_TYPE_BATTERY;
 	battery->test_mode = 0;
 	battery->factory_mode = false;
-#if defined(CONFIG_STORE_MODE)
-	battery->store_mode = true;
-#else
 	battery->store_mode = false;
-#endif
 	battery->slate_mode = false;
 	battery->is_hc_usb = false;
 
@@ -5804,8 +5809,6 @@ static void sec_battery_shutdown(struct device *dev)
 		= dev_get_drvdata(dev);
 	union power_supply_propval val;
 
-	dev_info(battery->dev, "%s: Start\n", __func__);
-
 	if (battery->pdata->always_enable) {
 		val.intval = 1;
 		psy_do_property(battery->pdata->charger_name, set,
@@ -5817,19 +5820,6 @@ static void sec_battery_shutdown(struct device *dev)
 		sec_bat_self_discharging_control(battery, false);
 	}
 #endif
-	switch (battery->pdata->polling_type) {
-		case SEC_BATTERY_MONITOR_WORKQUEUE:
-		cancel_delayed_work(&battery->polling_work);
-		break;
-	case SEC_BATTERY_MONITOR_ALARM:
-		alarm_cancel(&battery->polling_alarm);
-		break;
-	default:
-		break;
-	}
-
-	alarm_cancel(&battery->event_termination_alarm);
-	cancel_delayed_work_sync(&battery->monitor_work);
 
 	return;
 }
@@ -5879,3 +5869,4 @@ module_exit(sec_battery_exit);
 MODULE_DESCRIPTION("Samsung Battery Driver");
 MODULE_AUTHOR("Samsung Electronics");
 MODULE_LICENSE("GPL");
+
